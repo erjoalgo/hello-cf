da239d6d67a4f28ea2b545b7bd14303bfed735cc
Ernesto Alfonso <ernesto.alfonsogonzalez@ge.com>
2016-08-11 14:15:43 -0700

    add new hello-word endpoints
    ip echo
    env echo
    http-session stateful counter

--- src/hello_cf/handler.clj
+++ src/hello_cf/handler.clj
@@ -2,11 +2,40 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.util.response :as r]
             [clojure.tools.logging :as log]
             [ring.adapter.jetty :refer [run-jetty]]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/counter" {session :session}
+       (let [count   (:count session 0)]
+         (-> (r/response
+              (format "You accessed this page %d times (%s)"
+                      count session))
+             (assoc-in [:session :count] (inc count))
+             (assoc-in [:headers "Content-Type"] "text/plain"))))
+  (GET "/echo-ip" []
+       ;;from
+       ;;https://github.com/ring-clojure/ring/wiki/Concepts
+       (fn [req]
+       (log/infof "on echo ip: %s" req)
+         {:status 200
+          :headers {"Content-Type" "text/plain"}
+          :body (:remote-addr req)}))
+
+  (fn [req]
+    ;echo env variables
+    (when (.startsWith (:uri req) "/echo-env")
+      (let [uri (:uri req)
+            components (-> (clojure.string/split uri #"/") rest)
+            env-val (reduce get (System/getenv) (rest components))]
+        (log/infof "components: %s" components)
+        (when env-val
+          {:status 200
+           :headers {"Content-Type" "text/plain"}
+           :body (-> env-val pr-str)}))))
+
   (route/not-found "Not Found"))
 
 (def app
